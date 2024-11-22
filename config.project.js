137dcc36c948b2d7ea67989ffc5eb2ac86a29297
Aubin <aubin.didier@opendatasoft.com>
2024-11-22 14:00:52 +0100

    componant for formation

--- config.project.js
+++ config.project.js
@@ -1,11 +1,11 @@
 var project_config = require('./config.project');
 
 module.exports = {
-    ODS_PORTAL_DOMAIN_ID: 'userclub', // Replace by your DOMAIN ID
-    DEFAULT_DOMAIN_URL: 'https://userclub.opendatasoft.com/', // REPLACE BY YOUR DOMAIN URL
+    ODS_PORTAL_DOMAIN_ID: 'atih', // Replace by your DOMAIN ID
+    DEFAULT_DOMAIN_URL: 'https://atih.opendatasoft.com/', // REPLACE BY YOUR DOMAIN URL
 
     // List of pages, used by the gulp server and gulp update/compile commands.
     // the name/id/slug of the page MUST CORRESPOND to the page id on your Opendatasoft portal.
     // it must also correspond to the ejs file name and scss file name.
-    PAGES: ['dataviz','map'],
+    PAGES: ['mco'],
 };
